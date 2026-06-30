9a70b641068680b31ae97764df9b7f8f2d9ffa9b
Saritha12102004 <145677676+Saritha12102004@users.noreply.github.com>
2026-06-30 15:44:01 +0530

    Update main (2).py

--- main (2).py
+++ main (2).py
@@ -22,7 +22,7 @@ class BankAccount:
         print(f"Account balance for {self.__account_holder_name}: ${self.__account_balance}")
 
 # Creating an instance of the BankAccount class
-account = BankAccount("123456789", "LOGESH", 1000)
+account = BankAccount("123456789", "SASA", 1000)
 
 # Testing deposit and withdrawal functionality
 account.display_balance()
